4c3dacd9236acbadaf8c1d43f1a2c43b77cbef2d
zhuochen <zhuochen777@gmail.com>
2022-11-02 18:25:24 -0700

    add code for api endpoints and tests

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}@{}/{}".format('zhuochen', 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,7 +24,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -33,7 +33,123 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
 
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+        self.assertFalse(data['current_category'])
+        self.assertTrue(len(data['categories']))
+
+    def test_404_get_paginated_questions_invalid_page(self):
+        res = self.client().get('/questions?page=100')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+    # def test_delete_question(self):
+    #     res = self.client().delete('/questions/15')
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(data['success'], True)
+    #     self.assertTrue(data['delete_question_id'])
+    #     self.assertTrue(len(data['current_questions']))
+    #     self.assertTrue(data['total_num_questions'])
+
+    def test_delete_nonexisting_question(self):
+        res = self.client().delete('/questions/100')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
+
+    def test_create_question(self):
+        res = self.client().post('/questions', json={'question': 'test question', 'answer': 'test answer', 'category': 1, 'difficulty': 3})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['current_category'])
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+
+    def test_create_existing_question(self):
+        res = self.client().post('/questions/2', json={'question': 'test question', 'answer': 'test answer', 'category': 1, 'difficulty': 3})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'method not allowed')
+
+    def test_search_question(self):
+        res = self.client().post('/questions', json={'searchTerm': 'dutch'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertFalse(data['current_category'])
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+
+    def test_search_question_without_result(self):
+        res = self.client().post('/questions', json={'searchTerm': 'abcde'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertFalse(data['current_category'])
+        self.assertEqual(len(data['questions']), 0)
+        self.assertTrue(data['total_questions'])
+
+    def test_get_questions_basedOn_category(self):
+        res = self.client().get('/categories/1/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+        self.assertEqual(data['current_category'], 1)
+
+    def test_get_questions_basedOn_invalid_category(self):
+        res = self.client().get('/categories/100/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+    def test_get_quiz_question(self):
+        res = self.client().post('/quizzes', json={"previous_questions": [9], "quiz_category": {"id": 4, "category": "History"}})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['question'])
+        self.assertEqual(data['question']['category'], 4)
+
+    def test_get_quiz_question_invalid_jsonInput(self):
+        res = self.client().post('/quizzes', json={"previous_questions": [9], "quiz_category": {"category": "History"}})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
